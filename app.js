d8b04bb4aaf04468148b14b1457d770b18c734f0
jenil shingala <jenilshingala28@gmail.com>
2025-09-24 20:58:31 +0530

    cors

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: "*",
-    // origin: "https://pocket-buddy.vercel.app",
+    origin: [
+      "http://localhost:5173", // local dev
+      "https://pocket-buddy-frontend.vercel.app", // your vercel frontend
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
